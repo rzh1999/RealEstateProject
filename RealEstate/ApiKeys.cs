f720f2debb90d8313b88780dae6c5e8e44f0ee7d
Andrew K <drummer4life93@yahoo.com>
2020-07-13 14:46:05 -0500

    Added Realtor API that gets property ID.

--- RealEstate/ApiKeys.cs
+++ RealEstate/ApiKeys.cs
@@ -8,6 +8,7 @@ namespace RealEstate
     public static class ApiKeys
     {
         public static readonly string googleApi = "AIzaSyDTc9CZoLQXjaVDImDNBhSQYnbX6oOQeTs";
-       
+        public static readonly string realtorApi = "f7fc2e1e1bmsha8ce365290f1052p148c97jsn3e883c9cbc5a";
+
     }
 }
